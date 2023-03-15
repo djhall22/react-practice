19994c241957dad21f0bee91490b95a92e627082
djhall22 <djhallba@gmail.com>
2023-03-15 09:25:59 -0400

    working with useState

--- 01react-practice/src/App.js
+++ 01react-practice/src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 import Categories from "./Categories";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Dave");
+  const nameChange = () => {
+    const names = ["David", "Josh", "Randy", "Brad"];
+    const int = Math.floor(Math.random() * 4);
+    setName(names[int]);
+  };
+
   return (
     <section className="App">
       <Header className="Header" />
-      <Categories />
+      <Categories nameChange={nameChange} name={name} />
     </section>
   );
 }
--- 01react-practice/src/Categories.js
+++ 01react-practice/src/Categories.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { useState } from "react";
 
-const Categories = () => {
-  const [count, setCount] = useState(2);
-
-  function incrementCount() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  function decrementCount() {
-    setCount((prevCount) => prevCount - 1);
-  }
-
+const Categories = ({ nameChange, name }) => {
   return (
     <div>
-      <button onClick={incrementCount}> + </button>
-      <span>{count}</span>
-      <button onClick={decrementCount}> - </button>
+      <p> Hello {name}! </p>
+      <button onClick={nameChange}> Change Name </button>
     </div>
   );
 };
